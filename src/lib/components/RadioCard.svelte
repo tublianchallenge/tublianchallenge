e1dd7598f68e23ce9dfc960210a1e88fe97e6773
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-18 22:53:25 -0600

    MarquisTheCoder:  Sat Nov 18 22:53:25 CST 2023

--- src/lib/components/RadioCard.svelte
+++ src/lib/components/RadioCard.svelte
@@ -1,40 +1,37 @@
-<script lang="js">
-    import { onMount } from 'svelte';
-    export let description = ""
+<script lang="ts">
+
+        export let description = ""
     export let img = ""
     export let title = ""
-    export let group = undefined
-    export let value = undefined
+    export let group: number= 0
+    export let value: number= 0
 
-    export let checked = false;
+    export let checked = true;
     
     let classes = "radio-card-border-wrap p-[1px] bg-[#292929] rounded-[20px]"
 
-    onMount(async () => {
-		if (!checked) {
-            classes="radio-card-border-wrap p-[1px] bg-[#292929] rounded-[20px]"
+    // onMount(async () => {
+	// 	if (!checked) {
+    //         classes="radio-card-border-wrap p-[1px] bg-gradient-to-br from-[#F5C370] to-[#DB50B1] rounded-[20px]"
+    //     }else{ 
+    //         classes="radio-card-border-wrap p-[1px] bg-[#292929] rounded-[20px]"
+    //     }
     
-        }else{
-            classes="radio-card-border-wrap p-[1px] bg-gradient-to-br from-[#F5C370] to-[#DB50B1] rounded-[20px]"
-        }
-    
-	});
-
-    function handleClick(event) {
-    
-    if (checked == false) {
-        classes="radio-card-border-wrap p-[1px] bg-[#292929] rounded-[20px]"
-      return
+	// });
+    function getChecked(event){
+        checked = event.target.checked;
     }
-    checked = !checked;
-    classes="radio-card-border-wrap p-[1px] bg-gradient-to-br from-[#F5C370] to-[#DB50B1] rounded-[20px]"
-   
-    console.log("unchecked")
-    
-  }
+    function handleClick(){
+        if (!checked) {
+            return
+        }
+        checked = !checked;
+    }
 </script>
 
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <label on:click={handleClick} class={classes}>
 
     <div class=" bg-[#1E1E1E] w-[250px] h-[288px] py-[14px] px-[38px] flex flex-col border-1 rounded-[20px] relative hover:bg-[#292929] hover:cursor-pointer">
@@ -42,6 +39,7 @@
         <input checked={checked} type="radio" bind:group {value} class="absolute top-[14px] left-[212px]">
 
         <div class="flex flex-row justify-center max-h-[128px] w-full mt-[25px]">
+            <!-- svelte-ignore a11y-missing-attribute -->
             <img src="{img}"/>
         </div>
         <div class="flex justify-center w-full text-[#FEFEFE] mt-[10px] mb-[6px]">
