8e42537ee2e9898dbd412f89de49f0e50fb02e1a
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-19 04:22:07 -0600

    MarquisTheCoder:  Sun Nov 19 04:22:07 CST 2023

--- src/lib/components/RadioCard.svelte
+++ src/lib/components/RadioCard.svelte
@@ -1,92 +1,50 @@
 <script lang="ts">
-
-    import { onMount } from "svelte";
-    export let img = String(undefined)
-    export let title = String(undefined)
-    export let description = String(undefined)
-    export let group: number = Number(undefined)
-    export let value: number = Number(undefined)
-    export let checked: boolean= Boolean(undefined);
     
-    const radioCardColors = {
-        radio:{
-            active: "linear-gradient(#F5C370, #DD58AC)",
-            hover: "linear-gradient(to left, #1E1E1E, #1E1E1E)",
-            inactive: "linear-gradient(#transparent, transparent)"
-        },
-        border: {
-            active: "linear-gradient(#F5C370, #DD58AC)",
-            inactive:"linear-gradient(#292929, #292929)"
-        }
-    }
+    export let img = String(undefined);
+    export let title = String(undefined);
+    export let description = String(undefined);
+    export let group: number = Number(undefined);
+    export let value: number = Number(undefined);
 
-    let radioBackground: string = radioCardColors.radio.inactive
-    let radioCardBorder: string = radioCardColors.border.inactive;
+    let radioCards = [
+        { id: 1, checked: false, radioBackground: "transparent", radioCardBorder: "linear-gradient(#292929, #292929)" },
 
-    function initializeRadioState(){
-        if (checked == true) {
-            radioBackground = radioCardColors.radio.active
-            radioCardBorder = radioCardColors.border.active
-        }else{
-            radioBackground = radioCardColors.radio.inactive
-            radioCardBorder = radioCardColors.border.inactive
-        }
-    }
+        // Add more objects for each radio card
+    ];
 
-    onMount(async () => {
-		initializeRadioState()
-
-        setInterval((() =>{
-            if (checked == false) {
-            radioBackground = radioCardColors.radio.inactive
-            radioCardBorder = radioCardColors.border.inactive
-            return
-        }
-        }), 100);
-	});
-
-    function handleClick(){
-        if (checked == true) {
-            radioBackground = radioCardColors.radio.active
-            radioCardBorder = radioCardColors.border.active
-            return
-        }
-        checked = !checked;
-        radioBackground = radioCardColors.radio.inactive
-        radioCardBorder = radioCardColors.border.inactive
+    function selectRadioCard(card) {
+        radioCards = radioCards.map((rc) => {
+            if (rc.id === card.id) {
+                rc.checked = true;
+                rc.radioBackground = "linear-gradient(#F5C370, #DD58AC)";
+                rc.radioCardBorder = "linear-gradient(#F5C370, #DD58AC)";
+            } else {
+                rc.checked = false;
+                rc.radioBackground = "transparent";
+                rc.radioCardBorder = "linear-gradient(#292929, #292929)";
+            }
+            return rc;
+        });
     }
 </script>
 
-
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<label on:click={handleClick} class="radio-card-border-wrap p-[1px] rounded-[20px]"
-    style="background-image: {radioCardBorder};">
-
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-    <div
-    class="bg-[#1E1E1E] w-[250px] h-[288px] py-[14px] px-[38px] flex flex-col border-1 rounded-[20px] relative hover:bg-[#292929] hover:cursor-pointer">
-        
-        <div>
-            <input checked={checked} type="radio" bind:group {value} class="absolute top-[14px] left-[212px]" style="opacity: 0;">
-
-            <div class="radio-box absolute top-[14px] 
-            left-[212px] w-[24px] h-[24px] rounded-full
-             border border-1 border-[#292929]" style="background-image: {radioBackground};">
+{#each radioCards as card}
+    <label on:click={() => selectRadioCard(card)} class="radio-card-border-wrap p-[1px] rounded-[20px]"
+        style="background-image: {card.radioCardBorder};">
+        <div class="bg-[#1E1E1E] w-[250px] h-[288px] py-[14px] px-[38px] flex flex-col border-1 rounded-[20px] relative hover:bg-[#292929] hover:cursor-pointer">
+            {#if card.checked}
+                <div class="radio-box absolute top-[14px] left-[212px] w-[24px] h-[24px] rounded-full border border-1 border-[#292929]" style="background-image: {card.radioBackground};">
+                </div>
+            {/if}
+            <div class="flex flex-row justify-center max-h-[128px] w-full mt-[25px]">
+                <img src="{img}" />
+            </div>
+            <div class="flex justify-center w-full text-[#FEFEFE] mt-[10px] mb-[6px]">
+                <h1 class="font-[700] font-space-grotesk text-[24px] whitespace-nowrap leading-[34px]">{title}</h1>
+            </div>
+            <div class="flex flex-row justify-center w-full text-[#CFCFCF]">
+                <p class="font-space-grotesk text-center text-[16px] font-[500] text-[#CFCFCF] leading-[24px] w-[210px] shrink-0">{description}</p>
             </div>
         </div>
-        
-
-        <div class="flex flex-row justify-center max-h-[128px] w-full mt-[25px]">
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <img src="{img}"/>
-        </div>
-        <div class="flex justify-center w-full text-[#FEFEFE] mt-[10px] mb-[6px]">
-            <h1 class="font-[700] font-space-grotesk text-[24px] whitespace-nowrap leading-[34px]">{title}</h1>
-        </div>
-        <div class="flex flex-row justify-center w-full text-[#CFCFCF]">
-            <p class="font-space-grotesk text-center text-[16px] font-[500] text-[#CFCFCF]leading-[24px] w-[210px] shrink-0">{description}</p>
-        </div>
-    </div>
-</label>
+    </label>
+{/each}
--- src/routes/deshawn_playground/+page.svelte
+++ src/routes/deshawn_playground/+page.svelte
@@ -16,49 +16,9 @@
         title="Team Projects"
         img={"/img/checkbox/tublian-vector_team.png"}
         description="Hire developers for team projects."/>
-    
-        <RadioCard
-        bind:group
-        value={2}
-        checked={false}
-        title="Personal Projects"
-        img={"/img/checkbox/tublian-vector_personal.png"}
-        description="Hire developers for professional projects."/>
-    
-        <RadioCard
-        bind:group
-        value={3}
-        checked={false}
-        title="Recruiting"
-        img={"/img/checkbox/tublian-vector_recruiting.png"}
-        description="Recruit developers for outstanding companies."/>
+        
     </Category>
 
-    <Category category={"Checkbox's"}> 
-        <RadioCard
-        bind:group
-        value={1}
-        checked={true}
-        title="Team Projects"
-        img={"/img/checkbox/tublian-vector_team.png"}
-        description="Hire developers for team projects."/>
-    
-        <RadioCard
-        bind:group
-        value={2}
-        checked={false}
-        title="Personal Projects"
-        img={"/img/checkbox/tublian-vector_personal.png"}
-        description="Hire developers for professional projects."/>
-    
-        <RadioCard
-        bind:group
-        value={3}
-        checked={false}
-        title="Recruiting"
-        img={"/img/checkbox/tublian-vector_recruiting.png"}
-        description="Recruit developers for outstanding companies."/>
-    </Category>
 </div>
 
 <style>
