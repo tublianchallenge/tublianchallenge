a4917ff32c56f9f8ed05fb05b80a786d266d6022
Papi <gbemialihassan@gmail.com>
2023-11-19 09:04:08 +0200

    Components

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,10 @@
+<!-- src/components/LoginLink.svelte -->
+<script>
+    export let text;
+    export let href;
+  </script>
+  
+  <div class="text-white text-base leading-6 self-stretch -mr-5 mt-3 max-md:max-w-full">
+    <span class="text-zinc-400">{text}</span>
+    <a href="{href}" class="font-bold text-white">Log in</a>
+  </div>  
