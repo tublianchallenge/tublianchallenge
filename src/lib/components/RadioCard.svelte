fd0536a317482e319b221bfff9519bed31be36ea
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-18 23:40:48 -0600

    MarquisTheCoder:  Sat Nov 18 23:40:48 CST 2023

--- src/lib/components/RadioCard.svelte
+++ src/lib/components/RadioCard.svelte
@@ -43,7 +43,7 @@
 
     <div class="bg-[#1E1E1E] w-[250px] h-[288px] py-[14px] px-[38px] flex flex-col border-1 rounded-[20px] relative hover:bg-[#292929] hover:cursor-pointer">
   
-        <input checked={checked} type="radio" bind:group {value} class="absolute top-[14px] left-[212px]">
+        <input checked={checked} type="radio" bind:group {value} class="absolute top-[14px] left-[212px]" hidden>
 
         <div class="flex flex-row justify-center max-h-[128px] w-full mt-[25px]">
             <!-- svelte-ignore a11y-missing-attribute -->
