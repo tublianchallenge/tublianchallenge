d48192289022adedcefe93ca95b21bc729cce6d1
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-21 05:34:46 -0600

    MarquisTheCoder: working on postgress datetime function  Tue Nov 21 05:34:46 CST 2023

--- src/lib/stores/model.ts
+++ src/lib/stores/model.ts
@@ -1,9 +1,28 @@
 import { writable } from 'svelte/store';
 
 export let profile_group = writable(1);
+
+
+function postgresDatetime() : String{
+    let dateToStringConversion = new Date()
+        .toISOString()
+        .slice(0, 19)
+        .replace('T', ' ');
+    return dateToStringConversion;
+}
+
 export let viewModel = writable({
 
-    radioCards = [
+
+    user_profile: {
+        dateCreated: Date(),
+        firstName: String(undefined),
+        lastName: String(undefined),
+
+    
+    },
+
+    radioCards :[
         { id: 1, checked: false, radioBackground: "transparent", radioCardBorder: "linear-gradient(#292929, #292929)" },
         // Add more objects for each radio card
     ],
