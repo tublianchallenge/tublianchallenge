0e0c7a36c9c26c3683b30087a55a1e804c51ea8b
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-11-26 19:04:59 -0600

    MarquisTheCoder:  Sun Nov 26 19:04:59 CST 2023

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,8 @@
 
 <script>
-  import CheckboxCard from "$lib/components/CheckboxCard.svelte"
-</script>
+  </script>
 
 <h1 class="text-3xl font-bold underline font-space-grotesk">
-    Hello world!
-    <CheckboxCard/>
   </h1>
   
   <style lang="postcss">
