daf1d8fc442df4aa96bbaf84759d0165dc43d141
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-07 11:18:32 -0600

    MarquisTheCoder:  Thu Dec  7 11:18:32 CST 2023

--- src/routes/ayo_playground/Signup.svelte
+++ src/routes/ayo_playground/Signup.svelte
@@ -1,11 +1,11 @@
 <!-- src/routes/Signup.svelte -->
 <script>
-  import Input from '$components/Input.svelte';
-  import PasswordStrengthIndicators from '$components/PasswordStrengthIndicators.svelte';
   import CreateAccountButton from '$components/CreateAccountButton.svelte';
   import Divider from '$components/Divider.svelte';
-  import LoginLink from '$components/LoginLink.svelte';
   import FooterLinks from '$components/FooterLinks.svelte';
+  import Input from '$components/Input.svelte';
+  import LoginLink from '$components/LoginLink.svelte';
+  import PasswordStrengthIndicators from '$components/PasswordStrengthIndicators.svelte';
 
   export let images = {
     main: "https://cdn.builder.io/api/v1/image/assets/TEMP/690365f7-ce1b-4fbe-93e2-112245ade8fd?apiKey=1e8cd32825fa495abd6eed872c3cbf13&",
