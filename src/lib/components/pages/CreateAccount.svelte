0a2f0e82f68fbf50f67cf08965972ab14d7a7400
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-11 14:14:55 -0600

    MarquisTheCoder: "go the post working now figuring out supabase validation issues" | Mon Dec 11 14:14:55 CST 2023

--- src/lib/components/pages/CreateAccount.svelte
+++ src/lib/components/pages/CreateAccount.svelte
@@ -25,7 +25,7 @@
       <p class="text-white/80 font-space-grotesk">Creating account for <span class="text-[#4AA3FF]">@{$firstName} {$lastName}</span></p>
     </div>
     
-    <form action="?/signup" method="post" class="flex flex-col justify-start h-full w-full gap-[20px] items-start">
+    <form action="?/signUp" method="post" class="flex flex-col justify-start h-full w-full gap-[20px] items-start">
 
       <div class="flex flex-col items-start w-full">
         <input name="email" id="email" placeholder="Email" bind:value={$email} type="text" class="font-space-grotesk text-[#888888] w-full md:w-[405px] h-auto bg-[transparent] border-b-[1px] text-[18px] md:text-[20px] border-[#888888] placeholder-[#888888bb] py-4 pl-2 outline-none">
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,23 +4,39 @@ import { fail, redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
     signUp: async ({request, locals}) =>{
-        const body = Object.fromEntries(await request.formData())
+        const body = Object.fromEntries(await request.formData());
+
+        let email: string = body.email as string;
+        let password: string = body.password as string;
+       
+        if (!email || !password) {
+            return fail(400, {
+                error: "need username and password"
+            })
+        }
+
+        
+        console.log("attempting to sign up with supabase");
         const {data, error: err} = await locals.supabase.auth.signUp({
             email: body.email as string ,
             password: body.password as string
-
         });
+
 
         if(err){
             if(err instanceof AuthApiError && err.status == 400){
+            console.log("invalid email and password")
                 return fail(400,{
                     error: "Your email or password is invalid. Get over it."
-                }); 
-            }
+                });
+            }else{
+                console.error("server error", err)
 
-            return fail(500, {
-                error: "The server doesn't like you get over it."
-            })
+                return fail(500, {
+                    error: "The server doesn't like you get over it."
+                })
+            }
+           
         }
         throw redirect(303, "/")
 
