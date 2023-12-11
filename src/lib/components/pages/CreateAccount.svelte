1485c0573df4fa8c3a126b50a2b4098a4089c78f
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-10 20:44:24 -0600

    MarquisTheCoder: "" | Sun Dec 10 20:44:24 CST 2023

--- src/lib/components/pages/CreateAccount.svelte
+++ src/lib/components/pages/CreateAccount.svelte
@@ -1,41 +1,31 @@
 <script lang="ts">
-    import { enhance } from "$app/forms";
-
+    
   
     
   import Button from "$lib/components/buttons/Button.svelte";
   import AccountQuestion from "$lib/components/main/AccountQuestion.svelte";
   import Navbar from "$lib/components/main/Navbar.svelte";
   import { email, firstName, lastName, password } from "$lib/stores/model";
-  import { onMount } from 'svelte';
-  import { z } from "zod";
+
+  import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+  import { createClient } from '@supabase/supabase-js';
   
-const invalid_type_error = 'Invalid type provided for this field';
-const required_error = 'This field cannot be blank';
-
-export const SignUpSchema = z.object({
- fullName: z
-  .string({ invalid_type_error, required_error })
-  .min(1, 'Value is too short'),
- username: z
-  .string({ invalid_type_error, required_error })
-  .min(1, 'Value is too short'),
- email: z
-  .string({ invalid_type_error, required_error })
-  .email('Please provide a valid email')
-  .min(1, 'Value is too short'),
- password: z
-  .string({ invalid_type_error, required_error })
-  .min(6, 'Password is too short'),
-});
-  onMount(async () =>{
-    console.log(`first name is ${$firstName}`);
-  });
-
+  const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+  
+  async function signUp(event){
+    console.log($email, $password);
+    if(event.target){
+      const { data, error } = await supabase.auth.signUp({
+      email: event.target.email.value,
+      password: event.target.password.value,
+      });
+    }
+  }
   
   function updateCredentials(event){
-      $email = event.target.email.value;
-      $password = event.target.password.value;
+    event.preventDefault();
+    $email = event.target.email.value;
+    $password = event.target.password.value;
     }
 </script>
     
@@ -53,12 +43,13 @@ export const SignUpSchema = z.object({
       <p class="text-white/90 font-space-grotesk text-[20px] font-[700] mt-[20px]">Create Account</p>
       <p class="text-white/80 font-space-grotesk">Creating account for <span class="text-[#4AA3FF]">@{$firstName} {$lastName}</span></p>
     </div>
-    <form use:enhance class="flex flex-col justify-start h-full w-full gap-[20px] items-start">
+    
+    <form  on:submit|preventDefault={signUp}  class="flex flex-col justify-start h-full w-full gap-[20px] items-start">
 
       <div class="flex flex-col items-start w-full">
-        <input id="email" placeholder="Email" bind:value={$email} type="text" class="font-space-grotesk text-[#888888] w-full md:w-[405px] h-auto bg-[transparent] border-b-[1px] text-[18px] md:text-[20px] border-[#888888] placeholder-[#888888bb] py-4 pl-2 outline-none">
-  
-        <input id="password" placeholder="Password" type="password" bind:value={$password}  class="font-space-grotesk text-[#888888] w-full md:w-[405px] h-auto bg-[transparent] border-b-[1px] text-[18px] md:text-[20px] border-[#888888] placeholder-[#888888bb] py-4 pl-2 outline-none">
+        <input name="email" id="email" placeholder="Email" bind:value={$email} type="text" class="font-space-grotesk text-[#888888] w-full md:w-[405px] h-auto bg-[transparent] border-b-[1px] text-[18px] md:text-[20px] border-[#888888] placeholder-[#888888bb] py-4 pl-2 outline-none">
+ ; 
+        <input name="password" id="password" placeholder="Password" type="password" bind:value={$password}  class="font-space-grotesk text-[#888888] w-full md:w-[405px] h-auto bg-[transparent] border-b-[1px] text-[18px] md:text-[20px] border-[#888888] placeholder-[#888888bb] py-4 pl-2 outline-none">
       </div>
       
       <div class="w-full">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,6 @@
     import CreateAccount from "$lib/components/pages/CreateAccount.svelte";
     import GetStarted from "$lib/components/pages/GetStarted.svelte";
     import { page } from "$lib/stores/model";
-    import { createClient } from '@supabase/supabase-js';
-    
-    import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-    const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
-
     // $: $page, saveState();
   </script>
 
