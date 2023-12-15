d736758bae9bd9e1bd34f423df0976500258c18c
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-15 15:59:57 -0600

    MarquisTheCoder: "git" | Fri Dec 15 15:59:57 CST 2023

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+// import adapter from '@sveltejs/adapter-auto';
+// // import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter()
+//   },
+//   preprocess: vitePreprocess()
+// };
+// export default config;
+
+import node from '@sveltejs/adapter-node';
+const config {
+	kit: {
+		adapter: node(),
+    methodOverride:{
+      allowed: ['PATCH', 'DELETE']
+    },
+	}
 };
+
 export default config;
