8acf49c359eeb9f969b44e3778ca57ee06886b0e
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-15 14:18:33 -0600

    MarquisTheCoder: "" | Fri Dec 15 14:18:33 CST 2023

--- src/lib/components/pages/Stripe.svelte
+++ src/lib/components/pages/Stripe.svelte
@@ -1,17 +1,27 @@
 
 <div class="flex overflow-hidden absolute top-0 left-0 z-30 flex-row justify-center p-20 w-screen bg-transparent backdrop-blur-lg h-fit">
     <div id="paymentForm" class="w-full md:w-[580px] h-[1141px] bg-[#1E1E1E] rounded-[20px] p-[20px] flex flex-col gap-[40px]">
+        
         <div class="flex flex-row justify-between">
             <img src="/img/pages/tublian-full.png" alt="tublina logo" class="h-[24.91px]">
             <button><img src="/img/pages/x-button.png" alt=""></button>
         </div>
 
-        <div id="monthlyPlan" class="w-[508px] h-[175px] bg-[#292929] rounded-[10px]">
-            <div class="p-[20px] w-full h-[60px] flex flex-row justify-between">
+        <div id="monthlyPlan" class=" w-[508px] h-[175px] bg-[#292929] rounded-[10px]">
+            <div class="p-[20px] w-full h-[60px] flex flex-row justify-between border-b-[1px] border-[#414141]">
                 <p class="font-[700] text-xl font-space-grotesk text-[#FEFEFE]/90">Monthly Plan</p>
                 <p class="text-[#FDD649] font-space-grotesk">Change Plan</p>
             </div>
-            <div></div>
+            <div class="flex flex-row justify-start w-full">
+
+            </div>
+            <div class="p-[20px] justify-between flex flex-row w-full text-[#FEFEFE] font-space-grotesk">
+                <p class="w-[50%] text-xl font-[500]">Total:</p>
+                <div class="w-[50%] flex flex-row justify-end gap-[10px]">
+                     <div class="flex flex-row justify-center font-[500] items-center h-full text-[#B7B7B7]">USD</div>
+            <div class="text-3xl font-[700]">$49.99<span class="font-[700] text-lg text-[#B7B7B7]">/Month</span></div>
+                </div>
+            </div>
         </div>
         
     </div>
