f90912fd4bd933c967bce26d91fabef1ba84b75d
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-12-15 13:41:38 -0600

    MarquisTheCoder: "" | Fri Dec 15 13:41:38 CST 2023

--- src/lib/components/pages/Stripe.svelte
+++ src/lib/components/pages/Stripe.svelte
@@ -1,4 +1,18 @@
 
-<div class="flex overflow-hidden absolute top-0 left-0 z-30 flex-row justify-center items-center w-screen h-screen bg-transparent backdrop-blur-lg">
-    
+<div class="flex overflow-hidden absolute top-0 left-0 z-30 flex-row justify-center p-20 w-screen bg-transparent backdrop-blur-lg h-fit">
+    <div id="paymentForm" class="w-full md:w-[580px] h-[1141px] bg-[#1E1E1E] rounded-[20px] p-[20px] flex flex-col gap-[40px]">
+        <div class="flex flex-row justify-between">
+            <img src="/img/pages/tublian-full.png" alt="tublina logo" class="h-[24.91px]">
+            <button><img src="/img/pages/x-button.png" alt=""></button>
+        </div>
+
+        <div id="monthlyPlan" class="w-[508px] h-[175px] bg-[#292929] rounded-[10px]">
+            <div class="p-[20px] w-full h-[60px] flex flex-row justify-between">
+                <p class="font-[700] text-xl font-space-grotesk text-[#FEFEFE]/90">Monthly Plan</p>
+                <p class="text-[#FDD649] font-space-grotesk">Change Plan</p>
+            </div>
+            <div></div>
+        </div>
+        
+    </div>
 </div>
